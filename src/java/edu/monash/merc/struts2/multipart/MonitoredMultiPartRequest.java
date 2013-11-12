fcd2504d99d8f49bb1e230144a74be8e6547ec7b
yusimoncn@gmail.com <yusimoncn@gmail.com@74c28ad8-4957-904f-ed5b-81b4a16aa023>
2013-11-12 23:55:57 +0000

    updated the Spring from 3.0.5 to 3.1.1, and updated the Struts2 from 2.3.8 to 2.3.15.3 to fixed the security issues

--- src/java/edu/monash/merc/struts2/multipart/MonitoredMultiPartRequest.java
+++ src/java/edu/monash/merc/struts2/multipart/MonitoredMultiPartRequest.java
@@ -28,18 +28,7 @@
 
 package edu.monash.merc.struts2.multipart;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
+import com.opensymphony.xwork2.inject.Inject;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.RequestContext;
@@ -50,306 +39,322 @@ import org.apache.log4j.Logger;
 import org.apache.struts2.StrutsConstants;
 import org.apache.struts2.dispatcher.multipart.MultiPartRequest;
 
-import com.opensymphony.xwork2.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 /**
  * This is a multipart request handler, but utilises commons-filupload 1.2 and adds a listener to the file upload
  * request, so that the progress can be monitor by other requests.
- * 
+ * <p/>
  * This is a copy of JakartaMultiPartRequest with some extra code put in to handle the ProgressMonitor listener class.
  * Strangely enough, extending JakartaMultiPartRequest and overiding the methods didnt seem to work? However, it wasnt
  * important enough to look into why, so i just copied the code and pasted here.
- * 
  */
 public class MonitoredMultiPartRequest implements MultiPartRequest {
 
-	static final Logger log = Logger.getLogger(MultiPartRequest.class);
+    static final Logger log = Logger.getLogger(MultiPartRequest.class);
 
-	// maps parameter name -> List of FileItem objects
-	private Map<String, List<FileItem>> files = new HashMap<String, List<FileItem>>();
-	// maps parameter name -> List of param values
-	private Map<String, List<String>> params = new HashMap<String, List<String>>();
-	// any errors while processing this request
-	private List<String> errors = new ArrayList<String>();
+    // maps parameter name -> List of FileItem objects
+    private Map<String, List<FileItem>> files = new HashMap<String, List<FileItem>>();
+    // maps parameter name -> List of param values
+    private Map<String, List<String>> params = new HashMap<String, List<String>>();
+    // any errors while processing this request
+    private List<String> errors = new ArrayList<String>();
 
-	private long maxSize;
+    private long maxSize;
 
-	@Inject(StrutsConstants.STRUTS_MULTIPART_MAXSIZE)
-	public void setMaxSize(String maxSize) {
-		this.maxSize = Long.parseLong(maxSize);
-	}
+    @Inject(StrutsConstants.STRUTS_MULTIPART_MAXSIZE)
+    public void setMaxSize(String maxSize) {
+        this.maxSize = Long.parseLong(maxSize);
+    }
 
-	/**
-	 * Creates a new request wrapper to handle multi-part data using methods adapted from Jason Pell's multipart classes
-	 * (see class description).
-	 * 
-	 * @param saveDir
-	 *            the directory to save off the file
-	 * @param servletRequest
-	 *            the request containing the multipart
-	 * @throws java.io.IOException
-	 *             is thrown if encoding fails.
-	 */
-	@SuppressWarnings("unchecked")
-	public void parse(HttpServletRequest servletRequest, String saveDir) throws IOException {
-		DiskFileItemFactory fac = new DiskFileItemFactory();
-		// Make sure that the data is written to file
-		fac.setSizeThreshold(0);
-		if (saveDir != null) {
-			fac.setRepository(new File(saveDir));
-		}
+    /**
+     * Creates a new request wrapper to handle multi-part data using methods adapted from Jason Pell's multipart classes
+     * (see class description).
+     *
+     * @param saveDir        the directory to save off the file
+     * @param servletRequest the request containing the multipart
+     * @throws java.io.IOException is thrown if encoding fails.
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    public void parse(HttpServletRequest servletRequest, String saveDir) throws IOException {
+        DiskFileItemFactory fac = new DiskFileItemFactory();
+        // Make sure that the data is written to file
+        fac.setSizeThreshold(0);
+        if (saveDir != null) {
+            fac.setRepository(new File(saveDir));
+        }
 
-		ProgressMonitor monitor = null;
-		// Parse the request
-		try {
-			ServletFileUpload upload = new ServletFileUpload(fac);
-			upload.setSizeMax(maxSize);
+        ProgressMonitor monitor = null;
+        // Parse the request
+        try {
+            ServletFileUpload upload = new ServletFileUpload(fac);
+            upload.setSizeMax(maxSize);
 
-			monitor = new ProgressMonitor();
-			upload.setProgressListener(monitor);
-			servletRequest.getSession().setAttribute(ProgressMonitor.SESSION_PROGRESS_MONITOR, monitor);
+            monitor = new ProgressMonitor();
+            upload.setProgressListener(monitor);
+            servletRequest.getSession().setAttribute(ProgressMonitor.SESSION_PROGRESS_MONITOR, monitor);
 
-			List<FileItem> items = upload.parseRequest(createRequestContext(servletRequest));
+            List<FileItem> items = upload.parseRequest(createRequestContext(servletRequest));
 
-			for (FileItem item1 : items) {
-				FileItem item = item1;
+            for (FileItem item1 : items) {
+                FileItem item = item1;
 
-				if (log.isDebugEnabled()) {
-					log.debug("Found item " + item.getFieldName());
-				}
-				if (item.isFormField()) {
-					if (log.isDebugEnabled()) {
-						log.debug("Item is a normal form field");
-					}
-					List<String> values;
-					if (params.get(item.getFieldName()) != null) {
-						values = params.get(item.getFieldName());
-					} else {
-						values = new ArrayList<String>();
-					}
+                if (log.isDebugEnabled()) {
+                    log.debug("Found item " + item.getFieldName());
+                }
+                if (item.isFormField()) {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Item is a normal form field");
+                    }
+                    List<String> values;
+                    if (params.get(item.getFieldName()) != null) {
+                        values = params.get(item.getFieldName());
+                    } else {
+                        values = new ArrayList<String>();
+                    }
 
-					// note: see http://jira.opensymphony.com/browse/WW-633
-					// basically, in some cases the charset may be null, so
-					// we're just going to try to "other" method (no idea if this
-					// will work)
-					String charset = servletRequest.getCharacterEncoding();
-					if (charset != null) {
-						values.add(item.getString(charset));
-					} else {
-						values.add(item.getString());
-					}
-					params.put(item.getFieldName(), values);
-				} else {
-					if (log.isDebugEnabled()) {
-						log.debug("Item is a file upload");
-					}
-					List<FileItem> values;
-					if (files.get(item.getFieldName()) != null) {
-						values = files.get(item.getFieldName());
-					} else {
-						values = new ArrayList<FileItem>();
-					}
+                    // note: see http://jira.opensymphony.com/browse/WW-633
+                    // basically, in some cases the charset may be null, so
+                    // we're just going to try to "other" method (no idea if this
+                    // will work)
+                    String charset = servletRequest.getCharacterEncoding();
+                    if (charset != null) {
+                        values.add(item.getString(charset));
+                    } else {
+                        values.add(item.getString());
+                    }
+                    params.put(item.getFieldName(), values);
+                } else {
+                    if (log.isDebugEnabled()) {
+                        log.debug("Item is a file upload");
+                    }
+                    List<FileItem> values;
+                    if (files.get(item.getFieldName()) != null) {
+                        values = files.get(item.getFieldName());
+                    } else {
+                        values = new ArrayList<FileItem>();
+                    }
 
-					values.add(item);
-					files.put(item.getFieldName(), values);
-				}
-			}
-		} catch (FileUploadException e) {
-			e.printStackTrace();
-			if (monitor != null)
-				monitor.abort();
-			log.error(e);
-			errors.add(e.getMessage());
-		} catch (Exception e) {
-			e.printStackTrace();
-			if (monitor != null)
-				monitor.abort();
-		}
-	}
+                    values.add(item);
+                    files.put(item.getFieldName(), values);
+                }
+            }
+        } catch (FileUploadException e) {
+            e.printStackTrace();
+            if (monitor != null)
+                monitor.abort();
+            log.error(e);
+            errors.add(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (monitor != null)
+                monitor.abort();
+        }
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFileParameterNames()
-	 */
-	public Enumeration<String> getFileParameterNames() {
-		return Collections.enumeration(files.keySet());
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFileParameterNames()
+     */
+    @Override
+    public Enumeration<String> getFileParameterNames() {
+        return Collections.enumeration(files.keySet());
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getContentType(java.lang.String)
-	 */
-	public String[] getContentType(String fieldName) {
-		List<FileItem> items = files.get(fieldName);
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getContentType(java.lang.String)
+     */
+    @Override
+    public String[] getContentType(String fieldName) {
+        List<FileItem> items = files.get(fieldName);
 
-		if (items == null) {
-			return null;
-		}
+        if (items == null) {
+            return null;
+        }
 
-		List<String> contentTypes = new ArrayList<String>(items.size());
-		for (int i = 0; i < items.size(); i++) {
-			FileItem fileItem = items.get(i);
-			contentTypes.add(fileItem.getContentType());
-		}
+        List<String> contentTypes = new ArrayList<String>(items.size());
+        for (int i = 0; i < items.size(); i++) {
+            FileItem fileItem = items.get(i);
+            contentTypes.add(fileItem.getContentType());
+        }
 
-		return (String[]) contentTypes.toArray(new String[contentTypes.size()]);
-	}
+        return (String[]) contentTypes.toArray(new String[contentTypes.size()]);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFile(java.lang.String)
-	 */
-	public File[] getFile(String fieldName) {
-		List<FileItem> items = files.get(fieldName);
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFile(java.lang.String)
+     */
+    @Override
+    public File[] getFile(String fieldName) {
+        List<FileItem> items = files.get(fieldName);
 
-		if (items == null) {
-			return null;
-		}
+        if (items == null) {
+            return null;
+        }
 
-		List<File> fileList = new ArrayList<File>(items.size());
-		for (int i = 0; i < items.size(); i++) {
-			DiskFileItem fileItem = (DiskFileItem) items.get(i);
-			fileList.add(fileItem.getStoreLocation());
-		}
+        List<File> fileList = new ArrayList<File>(items.size());
+        for (int i = 0; i < items.size(); i++) {
+            DiskFileItem fileItem = (DiskFileItem) items.get(i);
+            fileList.add(fileItem.getStoreLocation());
+        }
 
-		return (File[]) fileList.toArray(new File[fileList.size()]);
-	}
+        return (File[]) fileList.toArray(new File[fileList.size()]);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFileNames(java.lang.String)
-	 */
-	public String[] getFileNames(String fieldName) {
-		List<FileItem> items = files.get(fieldName);
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFileNames(java.lang.String)
+     */
+    @Override
+    public String[] getFileNames(String fieldName) {
+        List<FileItem> items = files.get(fieldName);
 
-		if (items == null) {
-			return null;
-		}
+        if (items == null) {
+            return null;
+        }
 
-		List<String> fileNames = new ArrayList<String>(items.size());
-		for (int i = 0; i < items.size(); i++) {
-			DiskFileItem fileItem = (DiskFileItem) items.get(i);
-			fileNames.add(getCanonicalName(fileItem.getName()));
-		}
+        List<String> fileNames = new ArrayList<String>(items.size());
+        for (int i = 0; i < items.size(); i++) {
+            DiskFileItem fileItem = (DiskFileItem) items.get(i);
+            fileNames.add(getCanonicalName(fileItem.getName()));
+        }
 
-		return (String[]) fileNames.toArray(new String[fileNames.size()]);
-	}
+        return (String[]) fileNames.toArray(new String[fileNames.size()]);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFilesystemName(java.lang.String)
-	 */
-	public String[] getFilesystemName(String fieldName) {
-		List<FileItem> items = files.get(fieldName);
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getFilesystemName(java.lang.String)
+     */
+    @Override
+    public String[] getFilesystemName(String fieldName) {
+        List<FileItem> items = files.get(fieldName);
 
-		if (items == null) {
-			return null;
-		}
+        if (items == null) {
+            return null;
+        }
 
-		List<String> fileNames = new ArrayList<String>(items.size());
-		for (int i = 0; i < items.size(); i++) {
-			DiskFileItem fileItem = (DiskFileItem) items.get(i);
-			fileNames.add(fileItem.getStoreLocation().getName());
-		}
+        List<String> fileNames = new ArrayList<String>(items.size());
+        for (int i = 0; i < items.size(); i++) {
+            DiskFileItem fileItem = (DiskFileItem) items.get(i);
+            fileNames.add(fileItem.getStoreLocation().getName());
+        }
 
-		return (String[]) fileNames.toArray(new String[fileNames.size()]);
-	}
+        return (String[]) fileNames.toArray(new String[fileNames.size()]);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getParameter(java.lang.String)
-	 */
-	public String getParameter(String name) {
-		List<String> v = params.get(name);
-		if (v != null && v.size() > 0) {
-			return v.get(0);
-		}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getParameter(java.lang.String)
+     */
+    @Override
+    public String getParameter(String name) {
+        List<String> v = params.get(name);
+        if (v != null && v.size() > 0) {
+            return v.get(0);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getParameterNames()
-	 */
-	public Enumeration<String> getParameterNames() {
-		return Collections.enumeration(params.keySet());
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getParameterNames()
+     */
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return Collections.enumeration(params.keySet());
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getParameterValues(java.lang.String)
-	 */
-	public String[] getParameterValues(String name) {
-		List<String> v = params.get(name);
-		if (v != null && v.size() > 0) {
-			return (String[]) v.toArray(new String[v.size()]);
-		}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getParameterValues(java.lang.String)
+     */
+    @Override
+    public String[] getParameterValues(String name) {
+        List<String> v = params.get(name);
+        if (v != null && v.size() > 0) {
+            return (String[]) v.toArray(new String[v.size()]);
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getErrors()
-	 */
-	public List<String> getErrors() {
-		return errors;
-	}
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.apache.struts2.dispatcher.multipart.MultiPartRequest#getErrors()
+     */
+    @Override
+    public List<String> getErrors() {
+        return errors;
+    }
 
-	/**
-	 * Returns the canonical name of the given file.
-	 * 
-	 * @param filename
-	 *            the given file
-	 * @return the canonical name of the given file
-	 */
-	private String getCanonicalName(String filename) {
-		int forwardSlash = filename.lastIndexOf("/");
-		int backwardSlash = filename.lastIndexOf("\\");
-		if (forwardSlash != -1 && forwardSlash > backwardSlash) {
-			filename = filename.substring(forwardSlash + 1, filename.length());
-		} else if (backwardSlash != -1 && backwardSlash >= forwardSlash) {
-			filename = filename.substring(backwardSlash + 1, filename.length());
-		}
+    /**
+     * Returns the canonical name of the given file.
+     *
+     * @param filename the given file
+     * @return the canonical name of the given file
+     */
+    private String getCanonicalName(String filename) {
+        int forwardSlash = filename.lastIndexOf("/");
+        int backwardSlash = filename.lastIndexOf("\\");
+        if (forwardSlash != -1 && forwardSlash > backwardSlash) {
+            filename = filename.substring(forwardSlash + 1, filename.length());
+        } else if (backwardSlash != -1 && backwardSlash >= forwardSlash) {
+            filename = filename.substring(backwardSlash + 1, filename.length());
+        }
 
-		return filename;
-	}
+        return filename;
+    }
 
-	/**
-	 * Creates a RequestContext needed by Jakarta Commons Upload.
-	 * 
-	 * @param req
-	 *            the request.
-	 * @return a new request context.
-	 */
-	private RequestContext createRequestContext(final HttpServletRequest req) {
-		return new RequestContext() {
-			public String getCharacterEncoding() {
-				return req.getCharacterEncoding();
-			}
+    /**
+     * Creates a RequestContext needed by Jakarta Commons Upload.
+     *
+     * @param req the request.
+     * @return a new request context.
+     */
+    private RequestContext createRequestContext(final HttpServletRequest req) {
+        return new RequestContext() {
+            @Override
+            public String getCharacterEncoding() {
+                return req.getCharacterEncoding();
+            }
 
-			public String getContentType() {
-				return req.getContentType();
-			}
+            @Override
+            public String getContentType() {
+                return req.getContentType();
+            }
 
-			public int getContentLength() {
-				return req.getContentLength();
-			}
+            @Override
+            public int getContentLength() {
+                return req.getContentLength();
+            }
 
-			public InputStream getInputStream() throws IOException {
-				return req.getInputStream();
-			}
-		};
-	}
+            @Override
+            public InputStream getInputStream() throws IOException {
+                return req.getInputStream();
+            }
+        };
+    }
+   @Override
+    public void cleanUp() {
+        //do nothing
+    }
 
 }
